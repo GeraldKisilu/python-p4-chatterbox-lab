ca6a9fd6534a454446fdf39658c0d5e0d440c206
geraldkisilu <muligerald6@gmail.com>
2024-07-07 00:12:24 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -23,5 +23,32 @@ def get_message_by_id(id):
     message = Message.query.get_or_404(id)
     return jsonify(message.to_dict())
 
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.get_or_404(id)
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
+
 if __name__ == '__main__':
     app.run(port=5555)
