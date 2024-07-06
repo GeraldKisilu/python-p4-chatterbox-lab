7c755bfc986b3fc276f3436abbe8bf752d452903
geraldkisilu <muligerald6@gmail.com>
2024-07-07 00:11:36 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
-from flask_migrate import Migrate
-
+from flask_cors import CORS  # type: ignore
+from flask_migrate import Migrate  # type: ignore
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +13,15 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages/<int:id>', methods=['GET'])
+def get_message_by_id(id):
+    message = Message.query.get_or_404(id)
+    return jsonify(message.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from sqlalchemy import MetaData, func # type: ignore
+from sqlalchemy_serializer import SerializerMixin # type: ignore
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +12,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
